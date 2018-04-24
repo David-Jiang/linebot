aa0f9b8b727c84d4940f149987c981256b617226
Jiang <TingGuang.Jiang@acer.com>
2018-04-24 15:58:52 +0800

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const linebot = require('linebot');
 const express = require('express');
+const _ = require('lodash');
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
@@ -13,16 +14,37 @@ const linebotParser = bot.parser();
 
 app.post('/linewebhook', linebotParser);
 
-let userInfoArr = [];
+const userInfo = {
+	userId: '',
+	stockIdArr: []
+};
+
+let userInfoArr<userInfo> = [];
 
 bot.on('message', function (event) {
+	let userName = '';
+	let id = '';
 	event.source.profile().then(function (profile) {
-		userInfoArr.push(profile.userId);
-		return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+		if (!_.find(userInfoArr, function(o) { return o.userId == profile.userId; })) {
+			userInfo.userId = profile.userId;
+			userInfoArr.push(userInfo);
+		}
+		userName = profile.displayName;
+		id = profile.userId;
 	});
 	switch (event.message.type) {
 		case 'text':
-			event.reply("165156");
+			if (_.startsWith(event.message.text,'-a')) {
+				let stockId = event.message.text.replace('-a','').trim();
+				let index = _.findIndex(userInfo, function(o) { return o.userId === id; });
+				let temp = ...userInfo[index].stockIdArr;
+				if (stockId.length == 4 && !_.includes(temp, stockId)) {
+					temp.push(stockId);
+				}
+				userInfo[index].stockIdArr = temp;
+			} else {
+				event.reply('您好，' + userName + '能否為您效勞？');
+			}
 			break;
 		case 'sticker':
 			event.reply({
@@ -42,9 +64,11 @@ app.listen(process.env.PORT || 80, function () {
 	console.log('LineBot is running.');
 });
 
-setTimeout(function() {
-	bot.push(userInfoArr, {
-		type: 'text',
-		text: '目前股價:108'
+setInterval(function() {
+	_.forEach(userInfoArr, function(vo) {
+		bot.push(vo.userId, {
+			type: 'text',
+			text: '你訂閱股票代號為：' + _.join(vo.stockIdArr, ',')
+		});
 	});
 } ,10000);
