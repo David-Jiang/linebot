bc4d2644d198eb36b3d689438ee954ad3e44889f
Jiang <TingGuang.Jiang@acer.com>
2018-04-24 15:16:18 +0800

    make it better

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const app = express();
 
 const linebotParser = bot.parser();
 
-
 app.post('/linewebhook', linebotParser);
 
+let userInfoArr = [];
+
 bot.on('message', function (event) {
+	event.source.profile().then(function (profile) {
+		userInfoArr.push(profile.userId);
+		return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+	});
 	switch (event.message.type) {
 		case 'text':
 			event.reply("165156");
@@ -23,7 +28,7 @@ bot.on('message', function (event) {
 			event.reply({
 				type: 'sticker',
 				packageId: 1,
-				stickerId: 1
+				stickerId: 4
 			});
 			break;
 	default:
@@ -31,14 +36,15 @@ bot.on('message', function (event) {
 			break;
 
 	}
-	/* event.reply(event.message.text)
-	.then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	}); */
 });
 
 app.listen(process.env.PORT || 80, function () {
 	console.log('LineBot is running.');
 });
+
+setTimeout(function() {
+	bot.push(userInfoArr, {
+		type: 'text',
+		text: '目前股價:108'
+	});
+} ,10000);
