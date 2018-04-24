7da20b9c12e1b1373df1673a29f0eac0237e7e16
Jiang <TingGuang.Jiang@acer.com>
2018-04-24 14:14:15 +0800

    make it better

--- index.js
+++ index.js
@@ -11,18 +11,32 @@ const app = express();
 
 const linebotParser = bot.parser();
 
-app.get('/',function(req,res){
-    res.send('Hello World!');
-});
 
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
-	event.reply(event.message.text).then(function (data) {
+	switch (event.message.type) {
+		case 'text':
+			event.reply("165156");
+			break;
+		case 'sticker':
+			event.reply({
+				type: 'sticker',
+				packageId: 1,
+				stickerId: 1
+			});
+			break;
+	default:
+			event.reply('Unknow message: ' + JSON.stringify(event));
+			break;
+
+	}
+	/* event.reply(event.message.text)
+	.then(function (data) {
 		console.log('Success', data);
 	}).catch(function (error) {
 		console.log('Error', error);
-	});
+	}); */
 });
 
 app.listen(process.env.PORT || 80, function () {
