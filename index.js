2473f0c2fb869a4cbaea2062cfeca31db40b45c2
Jiang <TingGuang.Jiang@acer.com>
2018-04-27 18:49:55 +0800

    make it better

--- index.js
+++ index.js
@@ -21,11 +21,6 @@ app.listen(process.env.PORT || 80, function () {
 	console.log('LineBot is running.')
 })
 
-/* const userInfo = {
-	userId: '',
-	stockIdArr: [],
-	subscr: false
-}; */
 let userInfoArr = []
 
 const stockInfo = {
@@ -66,20 +61,15 @@ bot.on('message', function (event) {
 
 				} else if (_.startsWith(event.message.text,'-r')) {
 					let userInfo = _.find(userInfoArr, function(o) { return o.userId === profile.userId; })
-					if (event.message.text.trim().length > 2) {
-						let stockIdArrBySplit = event.message.text.replace('-r','').trim().split("-")
+					let stockIdArrBySplit = event.message.text.replace('-r','').trim().split("-")
 						_.forEach(stockIdArrBySplit , function(stockIdneedDel) { 
 							_.forEach(userInfo.stockIdArr, function(stockIdown) {
 								_.remove(userInfo.stockIdArr, function(n) {return stockIdneedDel == stockIdown})
 							})
 						})
-						event.reply('您好' + profile.displayName + "，股票代號:" +stockIdArrBySplit.join(',') + '，已移除推播清單')
-					} else {
-						userInfo.subscr = false
-						event.reply('您好' + profile.displayName + '，已暫停推播')
-					}
-				} else if (_.startsWith(event.message.text,'-c')) {
-					let stockIdArrBySplit = event.message.text.replace('-c','').trim().split("-")
+					event.reply('您好' + profile.displayName + "，股票代號:" +stockIdArrBySplit.join(',') + '，已移除推播清單')
+				} else if (_.startsWith(event.message.text,'-i')) {
+					let stockIdArrBySplit = event.message.text.replace('-i','').trim().split("-")
 					_.forEach(stockIdArrBySplit , function(stockId) { 
 						if (stockId.length == 4) {
 							addToStockList(stockId)
@@ -94,11 +84,22 @@ bot.on('message', function (event) {
 						}
 					})
 
+				} else if (_.startsWith(event.message.text,'-s') || _.startsWith(event.message.text,'-c')) {
+					let type = event.message.text.trim();
+					let userInfo = _.find(userInfoArr, function(o) { return o.userId === profile.userId; })
+					if (type == "-s") {
+						userInfo.subscr = true
+						event.reply('您好' + profile.displayName + '，已開啟推播')
+					} else {
+						userInfo.subscr = false
+						event.reply('您好' + profile.displayName + '，已暫停推播')
+					}
 				} else if (_.startsWith(event.message.text,'-v')) {
 					let showMessage = "";
 					showMessage += "輸入-a 股票代號 可定時推播該股票資訊\n\t(例如: -a 2353-2330)\n"
-					showMessage += "輸入-c 股票代號 可回應該股票資訊\n\t(例如: -c 2353-2330)\n"
-					showMessage += "輸入-r 可暫停推播\n"
+					showMessage += "輸入-i 股票代號 可回應該股票資訊\n\t(例如: -c 2353-2330)\n"
+					showMessage += "輸入-s 可開啟推播\n"
+					showMessage += "輸入-c 可暫停推播\n"
 					showMessage += "輸入-r 股票代號 可移除該股票資訊推播\n\t(例如: -r 2353-2330)\n"
 					showMessage += ""
 					event.reply(showMessage)
