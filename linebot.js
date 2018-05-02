455800521f5497fe878e3cdf063b68342439ded6
Jiang <TingGuang.Jiang@acer.com>
2018-05-02 17:01:03 +0800

    'yes'

--- linebot.js
+++ linebot.js
@@ -29,33 +29,9 @@ app.post('/linewebhook', line.middleware(config), (req, res) => {
 })
 const obj = {
   method: 'POST',
-  url: 'https://api.line.me/v2/bot/richmenu',
+  url: 'https://api.line.me/v2/bot/richmenu/list',
   headers: {
     Authorization: `Bearer <${process.env.CHANNEL_ACCESS_TOKEN}>`,
-    'Content-Type': 'application/json'
-  },
-  body: {
-    size: {
-      width: 2500,
-      height: 1686
-    },
-    selected: false,
-    name: 'Nice richmenu',
-    chatBarText: 'Tap here',
-    areas: [
-      {
-        bounds: {
-          x: 0,
-          y: 0,
-          width: 2500,
-          height: 1686
-        },
-        action: {
-          type: 'postback',
-          data: 'action=buy&itemid=123'
-        }
-      }
-   ]
   },
   json: true
 }
