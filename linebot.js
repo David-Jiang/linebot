373c01d98538d672e227e0299b21f35329bc6d36
Jiang <TingGuang.Jiang@acer.com>
2018-05-04 18:28:30 +0800

    '

--- linebot.js
+++ linebot.js
@@ -33,13 +33,13 @@ let stockList = [];
 const handleEvent = (event: any) => {
   switch (event.type) {
     case 'message':
-      switch (event.message.type) {
+      /* switch (event.message.type) {
         case 'text':
           return handleText(event.message.text, event.replyToken, event.source);
         default:
-          replyText(event.replyToken, '請輸入正確訊息唷');
-          break;
-      }
+          return replyText(event.replyToken, '請輸入正確訊息唷');
+      } */
+      return replyText(event.replyToken, '請輸入正確訊息唷');
     case 'postback':
       return handlePostback(event.postback, event.replyToken, event.source);
     default:
