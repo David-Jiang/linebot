21005686ec0d5319ca9adc525530897c12308473
Jiang <TingGuang.Jiang@acer.com>
2018-05-04 21:14:01 +0800

    '

--- linebot.js
+++ linebot.js
@@ -91,10 +91,10 @@ const handleText = (text: string, replyToken: any, source: any) => {
             addToStockList(stockId);
             let obj = stockList.find((o) => { return o.stockId === stockId && o.currPrice > 0; });
             if (obj) {
-              showMessage += `股票${obj.stockName}(${obj.stockId})\n
-目前價:${obj.currPrice}(${(obj.currPrice - obj.startPrice > 0 ? '+' : '')} ${returnFloat(obj.currPrice - obj.startPrice)} )\n
-最高價:${obj.hightPrice}\n
-最低價:${obj.lowPrice}\n`;
+              showMessage += `股票${obj.stockName}(${obj.stockId})
+目前價:${obj.currPrice}(${(obj.currPrice - obj.startPrice > 0 ? '+' : '')}${returnFloat(obj.currPrice - obj.startPrice)})
+最高價:${obj.hightPrice}
+最低價:${obj.lowPrice}`;
             }
           }
         });
@@ -113,11 +113,11 @@ const handleText = (text: string, replyToken: any, source: any) => {
           replyText(replyToken, `您好${userName}，已暫停推播`);
         }
       } else if (messageText.startsWith('-v')) {
-        let showMessage = '輸入-a 股票代號 可定時推播該股票資訊\n\t(例如: -a 2353-2330)\n';
-        showMessage += '輸入-i 股票代號 可回應該股票資訊\n\t(例如: -c 2353-2330)\n';
+        let showMessage = '輸入-a 股票代號 可定時推播該股票資訊\n\t(例如: -a 2327-2330)\n';
+        showMessage += '輸入-i 股票代號 可回應該股票資訊\n\t(例如: -i 2327-2330)\n';
         showMessage += '輸入-s 可開啟推播\n';
         showMessage += '輸入-c 可暫停推播\n';
-        showMessage += '輸入-r 股票代號 可移除該股票資訊推播\n\t(例如: -r 2353-2330)\n';
+        showMessage += '輸入-r 股票代號 可移除該股票資訊推播\n\t(例如: -r 2327-2330)\n';
         replyText(replyToken, showMessage);
       } else if (messageText.startsWith('只要')) {
         new Error('pass');
