2f69e86e2b82a644136dc9ddc1cfa647462e1bac
Jiang <TingGuang.Jiang@acer.com>
2018-05-06 13:57:39 +0800

    'twet'

--- linebot.js
+++ linebot.js
@@ -235,7 +235,7 @@ rp(reqOpt)
             console.log(`getStockInfo發生錯誤:${err}`);
           });
       }
-    }, 60000);
+    }, 60001);
   })
   .catch((err) => {
     console.log(`前導網頁get cookie發生錯誤:${err}`);
