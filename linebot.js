ddd1c46d0f2ef3bb8aa347c6de7368ac2a190efc
Jiang <TingGuang.Jiang@acer.com>
2018-06-20 16:17:34 +0800

    add stock cal

--- linebot.js
+++ linebot.js
@@ -15,12 +15,12 @@ app.listen(process.env.PORT || 80, () => {
   console.log('LineBot is running and Port is ' + process.env.PORT);
 });
 app.use(express.static(__dirname + '/'));
-app.get('/15', function (req, res) {
+/* app.get('/', function (req, res) {
   res.sendFile('/index.html');
-});
-app.get('/stockPrice', function (req, res) {
+}); */
+/* app.get('/stockPrice', function (req, res) {
   res.sendFile('/index.html');
-});
+}); */
 app.post('/linewebhook', line.middleware(config), (req, res) => {
   if (!Array.isArray(req.body.events)) {
     return res.status(500).end();
